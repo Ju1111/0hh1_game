eeaf415c32644a4968d64b772020db661f98915d
Ju111 <SchneiderJu@gmx.net>
2018-02-23 11:44:31 +0100

    setup redux store

---
+++ src/setupTests.js
@@ -0,0 +1,21 @@
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import 'jest-enzyme'
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+
+configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('wraps everything in a div tag', () => {
+    expect(app).toHaveTagName('div')
+    expect(app).toHaveClassName('App')
+  })
+
+  it('contains a title', () => {
+    expect(app.find('h1.title')).toHaveText('0hh1')
+  })
+})
